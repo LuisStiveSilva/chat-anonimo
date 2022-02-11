d38ee1dc103fbbf2df1e1de80f4a320fdea6fb8a
LuisStiveSilva <luis.silva.prudencio@gmail.com>
2022-02-10 23:13:19 -0500

    router con hash

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 
 ReactDOM.render(
-  <BrowserRouter basename="/chat-anonimo">
+  // <BrowserRouter basename="/chat-anonimo">
+  //   <App />
+  // </BrowserRouter>,
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
